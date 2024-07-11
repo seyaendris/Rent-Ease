c34cf5279c765761427a32506b3bbd4449a1d375
seyaendris <seide9894@gmail.com>
2024-07-12 00:53:01 +0300

    Avatar and UserMenu created

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 
 
 const Avatar = () => {
+    
   return (
     <Image 
         className="rounded-full"
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,10 +2,18 @@
 
 import { AiOutlineMenu } from "react-icons/ai"
 import Avatar from "../Avatar"
+import { useCallback, useState } from "react"
+import MenuItem from "./MenuItem"
 
 
 const UserMenu = () => {
-  return (
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, [])
+
+    return (
     <div className="relative">
         <div className="flex flex-row items-center gap-3">
             <div
@@ -16,7 +24,7 @@ const UserMenu = () => {
             </div>
 
             <div
-                onClick={() => {}}
+                onClick={toggleOpen}
                 className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
                 >
                     <AiOutlineMenu />
@@ -26,6 +34,25 @@ const UserMenu = () => {
 
             </div>
         </div>
+
+        {isOpen && (
+            <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+                <div className="flex flex-col cursor-pointer">
+                    <>
+                        <MenuItem 
+                            onClick={() => {}}
+                            label="Login"
+                            />
+                        <MenuItem 
+                            onClick={() => {}}
+                            label="Sign Up"
+                            />    
+                    </>
+
+                </div>
+
+            </div>
+        )}
 
     </div>
   )
