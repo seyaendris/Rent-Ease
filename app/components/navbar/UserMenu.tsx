7fb55a0ebb04c927a82494df9c42bbe1cda91328
seyaendris <seide9894@gmail.com>
2024-07-12 22:59:50 +0300

    RegisterModal component created

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,9 +4,11 @@ import { AiOutlineMenu } from "react-icons/ai"
 import Avatar from "../Avatar"
 import { useCallback, useState } from "react"
 import MenuItem from "./MenuItem"
+import useRegisterModal from "@/app/hooks/useRegisterModal"
 
 
 const UserMenu = () => {
+    const registerModal = useRegisterModal();
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleOpen = useCallback(() => {
@@ -44,7 +46,7 @@ const UserMenu = () => {
                             label="Login"
                             />
                         <MenuItem 
-                            onClick={() => {}}
+                            onClick={registerModal.onOpen}
                             label="Sign Up"
                             />    
                     </>
