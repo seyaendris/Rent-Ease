c1214768d45cbe768368d93161578652b964fada
Seid Endris <seide9892@gmail.com>
2024-08-02 21:19:05 +0300

    CountrySelct component updated

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import useCountries from '@/app/hooks/useCountries'
-import { DiVim } from 'react-icons/di'
 import Select from 'react-select'
 
 export type CountrySelectValue = {
@@ -17,38 +16,36 @@ interface CountrySelectProps {
     onChange: (value: CountrySelectValue) => void
 }
 
-
-const CountrySelect:React.FC<CountrySelectProps> = ({
+const CountrySelect: React.FC<CountrySelectProps> = ({
     value,
     onChange
 }) => {
     const { getAll } = useCountries()
 
-  return (
-    <div>
-        <Select 
-            placeholder="Anywhere"
-            isClearable
-            options={getAll()}
-            value={value}
-            onChange={(value) => onChange(value as CountrySelectValue)}
-            formatOptionLabel={(option: any) => (
-                <div className='flex flex-row items-center gap-3'>
-                    <div>
-                        {/* {option} */}
+    return (
+        <div>
+            <Select 
+                placeholder="Anywhere"
+                isClearable
+                options={getAll()}
+                value={value}
+                onChange={(value) => onChange(value as CountrySelectValue)}
+                formatOptionLabel={(option: any) => (
+                    <div className='flex flex-row items-center gap-3'>
+                        <div>
+                            {option.flag}
+                        </div>
+                        <div>
+                            {option.label},
+                            <span className='text-neutral-800 ml-1'>
+                                {option.region}
+                            </span>
+                        </div>
                     </div>
-                    <div>
-                        {option.label},
-                        <span className='text-neutral-800 ml-1'>
-                            {option.region}
-                        </span>
-                    </div>
-
-                </div>
-            )}
+                )}
             />
-    </div>
-  )
+        </div>
+    )
 }
 
 export default CountrySelect
