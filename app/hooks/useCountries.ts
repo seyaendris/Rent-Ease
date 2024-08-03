64f4177c25a82c0f86a06e0c4332edbc195b0045
Seid Endris <seide9892@gmail.com>
2024-08-03 15:33:00 +0300

    Finished the useCountries hook

--- app/hooks/useCountries.ts
+++ app/hooks/useCountries.ts
@@ -3,22 +3,22 @@ import countries from "world-countries";
 const formattedCountries = countries.map((country) => ({
     value: country.cca2,
     label: country.name.common,
-    flag: country.flag,
+    flag: country.cca2,  // Ensure this is the cca2 code
     latlng: country.latlng,
     region: country.region
-}))
+}));
 
 const useCountries = () => {
     const getAll = () => formattedCountries;
 
     const getByValue = (value: string) => {
-        return formattedCountries.find((item) => item.value === value)
-    }
+        return formattedCountries.find((item) => item.value === value);
+    };
 
     return {
         getAll,
         getByValue
-    }
-}
+    };
+};
 
-export default useCountries
+export default useCountries;
