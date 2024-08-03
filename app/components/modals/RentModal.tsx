28e5de991b29fd64c748742a2cca5b67895af2f8
Seid Endris <seide9892@gmail.com>
2024-08-03 15:31:13 +0300

    Integrate countryselect to the rent modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import { categories } from '../navbar/Categories'
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
 
 enum STEPS {
     CATEGORY = 0,
@@ -47,6 +48,7 @@ const RentModal = () => {
     })
 
     const category = watch('category')
+    const location = watch('location')
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -109,8 +111,11 @@ const RentModal = () => {
                     title="Where is your place located?"
                     subtitle="Help guest find you!"
                     />
-                    <CountrySelect 
+                    <CountrySelect
+                        value={location}
+                        onChange={(value) => setCustomValue('location', value)} 
                         />
+                    <Map />
             </div>
         )
     }
