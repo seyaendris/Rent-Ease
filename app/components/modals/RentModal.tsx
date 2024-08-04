6872991c6159fe8172e4bf87101673ddd386a753
seyaendris <seide9894@gmail.com>
2024-08-04 23:43:47 +0300

    Finished the Description OPtion

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
+import Input from "../inputs/Input";
 
 enum STEPS {
     CATEGORY = 0,
@@ -25,6 +26,7 @@ const RentModal = () => {
     const rentModal = useRentModal();
 
     const [step, setStep] = useState(STEPS.CATEGORY)
+    const [isLoading, setIsLoading] = useState(false)
 
     const {
         register,
@@ -177,7 +179,29 @@ const RentModal = () => {
 if(step === STEPS.DESCRIPTION) {
     bodyContent = (
         <div className="flex flex-col gap-8">
-
+            <Heading 
+                title="How would you describe your place?"
+                subtitle="Short and Sweet works best!"
+                />
+            <Input 
+                id="title"
+                label="Title"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+                type="text"
+                />
+                <hr />
+            <Input 
+                id="description"
+                label="Description"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+                type="text"
+                />
         </div>
     )
 }
