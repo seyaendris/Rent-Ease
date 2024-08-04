a01a080de2f15727ef6cd3ba2376e1d28972f421
seyaendris <seide9894@gmail.com>
2024-08-05 00:20:10 +0300

    created the onSubmit functionality

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,12 +6,15 @@ import { useMemo, useState } from "react";
 import Heading from '../Heading';
 import { categories } from '../navbar/Categories'
 import CategoryInput from "../inputs/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
 import Input from "../inputs/Input";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 enum STEPS {
     CATEGORY = 0,
@@ -24,6 +27,7 @@ enum STEPS {
 
 const RentModal = () => {
     const rentModal = useRentModal();
+    const router = useRouter();
 
     const [step, setStep] = useState(STEPS.CATEGORY)
     const [isLoading, setIsLoading] = useState(false)
@@ -75,6 +79,29 @@ const RentModal = () => {
     }
     const onNext = () => {
         setStep((value) => value + 1);
+    }
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        if(step !== STEPS.PRICE) {
+            return onNext();
+        }
+
+        setIsLoading(true);
+
+        axios.post('/api/listings', data)
+        .then(() => {
+            toast.success('Listing Created')
+            router.refresh();
+            reset();
+            setStep(STEPS.CATEGORY);
+            rentModal.onClose();
+        })
+        .catch(() => {
+            toast.error('Something went wrong')
+        })
+        .finally(() => {
+            setIsLoading(false)
+        })
     }
 
     const actionLabel = useMemo(() => {
@@ -231,7 +258,7 @@ if(step === STEPS.PRICE) {
         <Modal
             isOpen={rentModal.isOpen} 
             onClose={rentModal.onClose}
-            onSubmit={onNext} 
+            onSubmit={handleSubmit(onSubmit)} 
             title="Rent your Home!"
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
