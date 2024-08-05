0dfd9d178fcf9996435192df530c98b992fc30b5
seyaendris <seide9894@gmail.com>
2024-08-05 17:03:34 +0300

    created the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
+  const isEmpty = true
+
+  if(isEmpty)  {
+    return (
+      <EmptyState showReset />
+    )
+  }
+
+
   return (
-    <main>
-      <div className="text-rose-500 text-2xl">Hello Airbnb</div>
-    </main>
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          <div>My Future Listing</div>
+      </div>
+    </Container>
   );
 }
