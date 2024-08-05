60818396e6ca97b1674554b59135e0c0a7b07661
seyaendris <seide9894@gmail.com>
2024-08-05 22:51:10 +0300

    created useFavorite custom hook

---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { SafeUser } from "../types";
+import useLoginModal from "./useLoginModal";
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import LoginModal from '../components/modals/LoginModal';
+import { useCallback, useMemo } from "react";
+import toast from "react-hot-toast";
+
+
+interface IUseFavorite {
+    listingId: string
+    currentUser?: SafeUser | null
+}
+
+const useFavorite = ({
+    listingId,
+    currentUser
+}: IUseFavorite) => {
+    const router = useRouter()
+    const LoginModal = useLoginModal()
+
+    const hasFavorited = useMemo(() => {
+        const list = currentUser?.favoriteIds || []
+
+        return list.includes(listingId)
+    }, [currentUser, listingId])
+
+    const toggleFavorite = useCallback(async (
+        e: React.MouseEvent<HTMLDivElement>
+    ) => {
+        e.stopPropagation()
+
+        if(!currentUser) {
+            return LoginModal.onOpen()
+        }
+
+        try {
+            let request
+
+            if(hasFavorited) {
+                request = () => axios.delete(`/api/favorities/${listingId}`)
+            }
+            else {
+                request = () => axios.post(`/api/favorities/${listingId}`)
+            }
+
+            await request()
+            router.refresh()
+            toast.success('Success')
+        } catch (error) {
+            toast.error('Something went erong')
+        }
+}, [currentUser, hasFavorited, LoginModal, listingId, router])
+
+return {
+    hasFavorited,
+    toggleFavorite
+}
+}
+
+export default useFavorite
