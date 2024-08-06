290cb1393001ea4a395d8a70e32b4bd3088f6414
seyaendris <seide9894@gmail.com>
2024-08-06 14:59:34 +0300

    updated the Map component

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -27,7 +27,7 @@ const Map:React.FC<MapProps> = ({
         center={center as L.LatLngExpression || [51, -0.09]}
         zoom={center ? 4 : 2}
         scrollWheelZoom={false}
-        className='h-[35vh] rounded-lg'
+        className='h-[50vh] md:h-[70vh] rounded-lg'
         >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
