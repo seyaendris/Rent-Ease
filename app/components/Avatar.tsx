86d6bb9911a88f184d733b65e68d87b653a5b4cf
seyaendris <seide9894@gmail.com>
2024-08-07 13:17:24 +0300

    updated the Avatar component

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -12,11 +12,12 @@ const Avatar: React.FC<AvatarProps> = ({
     
   return (
     <Image 
-        className="rounded-full"
+        className="rounded-full w-12 md:w-7"
         height={30}
         width={30}
         alt="Avatar"
         src={src || "/images/user.png"}
+        
         />
   )
 }
