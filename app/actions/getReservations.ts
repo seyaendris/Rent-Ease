da7ae58758f5f089204f2e0b2db05e7914593132
seyaendris <seide9894@gmail.com>
2024-08-07 13:16:33 +0300

    updated the getReservation server action

--- app/actions/getReservations.ts
+++ app/actions/getReservations.ts
@@ -24,7 +24,7 @@ export default async function getReservations(
         }
     
         if(authorId) {
-            query.authorId = { userId: authorId }
+            query.listing = { userId: authorId }
         }
     
         const reservations = await prisma.reservation.findMany({
