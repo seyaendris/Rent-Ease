2545ac42787530d1df8ef7e5217e8d62a336f911
seyaendris <seide9894@gmail.com>
2024-08-07 13:50:22 +0300

    updated the user menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -74,7 +74,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                          label="My Trips"
                          />
                      <MenuItem 
-                         onClick={() => {}}
+                         onClick={() => router.push('/favorites')}
                          label="My Favorite"
                          /> 
                      <MenuItem 
