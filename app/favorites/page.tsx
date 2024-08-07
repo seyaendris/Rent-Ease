d4859871fbf8f5bd49bcb0e923eb7595b2358581
seyaendris <seide9894@gmail.com>
2024-08-07 14:08:25 +0300

    Finished the Favorites page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import getCurrentUser from '../actions/getCurrentUser'
 import EmptyState from '../components/EmptyState'
+import getFavoriteListings from '../actions/getFavoriteListings'
+import FavoriteClient from './FavoriteClient'
 
-const ListingPage = () => {
+const ListingPage = async () => {
+    const currentUser = await getCurrentUser()
+    const listings = await getFavoriteListings()
+
+    if(listings.length === 0) {
+        return (
+            <EmptyState 
+                title='No Favorites found!'
+                subtitle='Looks like you did not get your favorite place'
+                />
+        )
+    }
+
     return (
-        <EmptyState 
-            title='No Favorites found!'
-            subtitle='Looks like you did not get your favorite place'
+        <FavoriteClient 
+            listings={listings}
+            currentUser={currentUser}
             />
     )
+    
 }
 
 export default ListingPage
