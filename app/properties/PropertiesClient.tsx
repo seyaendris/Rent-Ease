e828c6de31697b94988a445e9bf48283cbad0eb8
seyaendris <seide9894@gmail.com>
2024-08-07 14:44:44 +0300

    created properties page

---
+++ app/properties/PropertiesClient.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import Container from "../components/Container"
+import Heading from "../components/Heading"
+import { SafeListing, SafeReservation, SafeUser } from "../types"
+import { useCallback, useState } from "react"
+import axios from "axios"
+import toast from "react-hot-toast"
+import ListingCard from "../components/listings/ListingCard"
+
+interface PropertiesClientProps {
+    listings: SafeListing[]
+    currentUser: SafeUser | null
+}
+
+
+const PropertiesClient:React.FC<PropertiesClientProps> = ({
+    listings,
+    currentUser
+}) => {
+    const router = useRouter()
+    const [deletingId, setDeletingId] = useState('')
+
+
+    const onCancel = useCallback((id: string) => {
+        setDeletingId(id)
+
+        axios.delete(`/api/listings/${id}`)
+        .then(() => {
+            toast.success('Property Removed')
+            router.refresh()
+        })
+        .catch((error) => {
+            toast.error(error?.response?.data?.error)
+        })
+        .finally(() => {
+            setDeletingId('')
+        })
+    }, [router])
+
+  return (
+    <Container>
+        <Heading 
+            title="Properties"
+            subtitle="List of your properties"
+            />
+
+        <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+            {listings.map((listings) => (
+                <ListingCard 
+                    key={listings.id}
+                    data={listings}
+                    actionId={listings.id}
+                    onAction={onCancel}
+                    disabled={deletingId === listings.id}
+                    actionLabel="Delete Property"
+                    currentUser={currentUser}
+                    />
+            ))}
+        </div>
+      
+    </Container>
+  )
+}
+
+export default PropertiesClient
---
+++ app/properties/page.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import getCurrentUser from '../actions/getCurrentUser'
+import EmptyState from '../components/EmptyState'
+import getReservations from '../actions/getReservations'
+import TripsClient from './PropertiesClient'
+import getListings from '../actions/getListings'
+import PropertiesClient from './PropertiesClient'
+
+const PropertiesPage = async () => {
+    const currentUser = await getCurrentUser()
+
+    if(!currentUser) {
+        return (
+            <EmptyState 
+                title='Unauthorized'
+                subtitle='Please Login'
+                />
+        )
+    }
+
+    const listings = await getListings({
+        userId: currentUser.id
+    })
+
+    if(listings.length === 0) {
+        return (
+            <EmptyState 
+                title='No Properties Found'
+                subtitle='Looks like you have no properties'
+                />
+        )
+    }
+
+    return (
+        <PropertiesClient 
+            listings={listings}
+            currentUser={currentUser}
+            />
+    )
+ 
+}
+
+export default PropertiesPage
