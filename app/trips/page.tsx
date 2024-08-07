0e8d1cb144eba55b4f39b4b4ab3072d04a00d61d
seyaendris <seide9894@gmail.com>
2024-08-07 13:25:11 +0300

    created Trips home page

---
+++ app/trips/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import getCurrentUser from '../actions/getCurrentUser'
+import EmptyState from '../components/EmptyState'
+import getReservations from '../actions/getReservations'
+import TripsClient from './TripsClient'
+
+const TripsPage = async () => {
+    const currentUser = await getCurrentUser()
+
+    if(!currentUser) {
+        return (
+            <EmptyState 
+                title='Unauthorized'
+                subtitle='Please Login'
+                />
+        )
+    }
+
+    const reservations = await getReservations({
+        userId: currentUser.id
+    })
+
+    if(reservations.length === 0) {
+        return (
+            <EmptyState 
+                title='No Trips Found'
+                subtitle='Looks like you have not reserved any trips'
+                />
+        )
+    }
+
+    return (
+        <TripsClient 
+            reservations={reservations}
+            currentUser={currentUser}
+            />
+    )
+ 
+}
+
+export default TripsPage
