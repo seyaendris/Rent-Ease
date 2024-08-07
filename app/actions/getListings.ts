b84e0267f9b7e09d7bded331d8b83eed2ece6d60
seyaendris <seide9894@gmail.com>
2024-08-07 17:55:59 +0300

    app/actions/getListings.ts

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,6 +2,13 @@ import prisma from '@/app/libs/prismadb'
 
 export interface IListingParams {
     userId?: string
+    guestCount?: number
+    roomCount?: number
+    bathroomCount?: number
+    startDate?: string
+    endDate?: string
+    locationValue?: string
+    category?: string
 }
 
 export default async function getListings(
@@ -9,13 +16,61 @@ export default async function getListings(
 ) {
     try {
 
-        const { userId } = params
+        const { userId, guestCount, roomCount,bathroomCount, startDate, endDate, locationValue, category } = params
 
         let query: any = {}
 
         if(userId) {
             query.userId = userId
         }
+
+        if(guestCount) {
+            query.guestCount = {
+                gte: +guestCount
+            }
+        }
+
+        if(roomCount) {
+            query.roomCount = {
+                gte: +roomCount
+            }
+        }
+
+        if(bathroomCount) {
+            query.bathroomCount = {
+                gte: +bathroomCount
+            }
+        }
+
+        if(locationValue) {
+            query.locationValue = locationValue
+        }
+
+        if(category) {
+            query.category = category
+        }
+
+        if(startDate && endDate) {
+            query.NOT = {
+                reservations: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: { gte: startDate },
+                                startDate: { lte: startDate},
+                            },
+
+                            {
+                                startDate: { lte: endDate },
+                                endDate: { gte: endDate }
+                            }
+
+                        ]
+                    }
+                }
+            }
+        }
+
 
         const listings = await prisma.listing.findMany({
             where: query,
