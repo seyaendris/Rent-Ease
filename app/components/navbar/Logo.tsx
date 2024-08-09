85152dfd0bf1e50e909fe27e958365dd944c48fe
seyaendris <seide9894@gmail.com>
2024-08-09 20:22:21 +0300

    navbar updated

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,14 +7,24 @@ import { useRouter } from "next/navigation"
 const Logo = () => {
     const router = useRouter();
   return (
+    <>
     <Image 
        onClick={() => router.push('/')} 
        alt="Logo"
        className="hidden md:block cursor-pointer" 
        height="100"
-       width="150"
-       src="/images/Logo.png"
+       width="110"
+       src="/images/logo2.png"
     />
+    <Image 
+       onClick={() => router.push('/')} 
+       alt="Logo"
+       className=" md:hidden cursor-pointer" 
+       height="50"
+       width="50"
+       src="/images/mobilelogo.png"
+    />
+    </>
   )
 }
 
