57522eb0315ebbf181f068cee3249ec15879538e
seyaendris <seide9894@gmail.com>
2024-08-09 11:35:33 +0300

    updated reservation

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -1,41 +1,41 @@
-import getCurrentUser from '../actions/getCurrentUser'
-import EmptyState from '../components/EmptyState'
-import getReservations from '../actions/getReservations'
-import ReservationsClient from './ReservationsClient'
-// import ReservationsClient from './ReservationsClient'
+export const dynamic = 'force-dynamic';
 
+import getCurrentUser from '../actions/getCurrentUser';
+import EmptyState from '../components/EmptyState';
+import getReservations from '../actions/getReservations';
+import ReservationsClient from './ReservationsClient';
 
 const ReservationsPage = async () => {
-    const currentUser = await getCurrentUser()
+    const currentUser = await getCurrentUser();
 
-    if(!currentUser) {
+    if (!currentUser) {
         return (
             <EmptyState 
                 title='Unauthorized'
                 subtitle='Please Login'
-                />
-        )
+            />
+        );
     }
 
     const reservations = await getReservations({
         authorId: currentUser.id
-    })
+    });
 
-    if(reservations.length === 0) {
+    if (reservations.length === 0) {
         return (
             <EmptyState 
                 title='No Reservations Found'
                 subtitle='Looks like you do not have any reservations on your properties'
-                />
-        )
+            />
+        );
     }
 
     return (
         <ReservationsClient 
             reservations={reservations}
             currentUser={currentUser}
-            />
-    )
-}
+        />
+    );
+};
 
-export default ReservationsPage
+export default ReservationsPage;
