b88a2ebc9163f4af2d069599fd69aeafa901af0f
seyaendris <seide9894@gmail.com>
2024-08-15 22:51:31 +0300

    updated the Login modal

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -2,8 +2,6 @@
 
 
 import { signIn } from 'next-auth/react'
-import axios from 'axios'
-import { AiFillGithub } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
 import { useCallback, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
